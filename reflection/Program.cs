67b77e2bf3c673592bd8016664e646946d6933ac
Pariya Sayadi <pariyaSayadi@outlook.com>
2023-08-28 09:14:00 +0330

    Add project files.

---
+++ reflection/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+
+class MyClass
+{
+    public void MyMethod()
+    {
+        Console.WriteLine("Hello from MyMethod!");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Type type = typeof(MyClass);
+        object instance = Activator.CreateInstance(type);
+
+        MethodInfo method = type.GetMethod("MyMethod");
+        method.Invoke(instance, null);
+    }
+}
